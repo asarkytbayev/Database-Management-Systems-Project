b32c7922e3ee099e6d94bf268d2c9d832ce1c0ae
yitonghu <hu.yit@husky.neu.edu>
2018-11-24 17:37:10 -0800

    Update Helper.java

--- Helper.java
+++ Helper.java
@@ -17,26 +17,26 @@ public class Helper {
 		return sku.matches("[A-Z]{2}-(\\d{6})-([\\d{2}]|[A-Z]{2}|[\\d][A-Z]|[A-Z][\\d])$");
 	}
 	
-//	/**
-//	 * This function parses a number string into an unsigned decimal integer. The characters 
-//	 * in the string must all be decimal digits. 
-//	 *
-//	 * @param s the number string
-//	 * @return the unsigned integer value represented by the argument in decimal
-//	 * @throws NumberFormatException if the string does not contain a parsable unsigned integer 
-//	 */
-//	static public int parseNumber(String s) throws NumberFormatException { // no need to parse
-//		return Integer.parseUnsignedInt(s);
-//	}
+	/**
+	 * This function parses a number string into an unsigned decimal integer. The characters 
+	 * in the string must all be decimal digits. 
+	 *
+	 * @param s the number string
+	 * @return the unsigned integer value represented by the argument in decimal
+	 * @throws NumberFormatException if the string does not contain a parsable unsigned integer 
+	 */
+	static public int parseNumber(String s) throws NumberFormatException { // need to parse
+		return Integer.parseUnsignedInt(s);
+	}
 	
-//	/**
-//	 * This function parses a price string into a float. 
-//	 * 
-//	 * @param s the price string
-//	 * @return the float value represented by the string argument
-//	 * @throws NumberFormatException if the string does not contain a parsable float
-//	 */
-//	static public double parsePrice(String s) throws NumberFormatException { // no need, sql will do the work for us!
-//		return Math.round(Double.parseDouble(s) * 100) / 100; // round up the double to 2 decimal places
-//	}
+	/**
+	 * This function parses a price string into a float. 
+	 * 
+	 * @param s the price string
+	 * @return the float value represented by the string argument
+	 * @throws NumberFormatException if the string does not contain a parsable float
+	 */
+	static public double parsePrice(String s) throws NumberFormatException { // need to parse
+		return Math.round(Double.parseDouble(s) * 100) / 100; // round up the double to 2 decimal places
+	}
 }
