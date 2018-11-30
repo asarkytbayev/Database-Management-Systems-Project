07edd886a1992aabbebfbf8b860b3aae975dbd98
Azamat Sarkytbayev <a.sarkytbayev@gmail.com>
2018-11-30 15:44:45 -0800

    updated OrderManager

--- OrderManager.java
+++ OrderManager.java
@@ -1,17 +1,22 @@
+package edu.northeastern.cs_5200;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+
+
 /**
  * This program creates a manager database for the ER data model
- * for OrderManager. There are entity tables for Product, Customer,
- * and InventoryRecord, and relationship tables for TheOrder and
+ * for OrderManager. 
+ * There are entity tables for Product, Customer, TheOrder
+ * and InventoryRecord, and relationship tables for 
  * OrderRecord relations in the ER model. 
  *
  * 
- *
+ * @author Azamat Sarkytbayev
  * @author Yitong Hu
  */
 public class OrderManager {
@@ -19,30 +24,30 @@ public class OrderManager {
 	public static void main(String[] args) {
 		// the default framework is embedded
 	    String protocol = "jdbc:derby:";
-	    String dbName = "manager";
+	    String dbName = "orderManager";
 		String connStr = protocol + dbName+ ";create=true";
 		
 		// triggers created by this program
-		String dbTriggers[] = {
+		String[] dbTriggers = {
 			"OrderRecordInsert"
 		};
 
 	    // tables created by this program
-		String dbTables[] = {
-			"TheOrder", "OrderRecord",		// relations
-			"Product", "Customer", "InventoryRecord" 	 // entities
-    	    };
+		String[] dbTables = {
+			"OrderRecord", "TheOrder",
+			"Customer", "InventoryRecord", "Product"
+	    };
 
-	    // functions created by this program
-	    String dbFunctions[] = {
+	    // stored functions created by this program
+	    String[] dbFunctions = {
 	    		"isSKU" // for check purpose
 	    };
 
 		Properties props = new Properties(); // connection properties
         // providing a user name and password is optional in the embedded
         // and derbyclient frameworks
-        props.put("user", "user2");
-        props.put("password", "user2");
+        props.put("user", "user1");
+        props.put("password", "user1");
 
 		try (
 	        // connect to the database using URL
@@ -50,7 +55,7 @@ public class OrderManager {
 
 	        // statement is channel for sending commands thru connection
 	        Statement stmt = conn.createStatement();
-		){
+		) {
 	        System.out.println("Connected to and created database " + dbName);
 	        
 	        // drop the database triggers and recreate them below
@@ -77,7 +82,7 @@ public class OrderManager {
             for (String fn : dbFunctions) {
 	            try {
 	            		stmt.executeUpdate("DROP FUNCTION " + fn);
-	            		System.out.println("Dropped function " + fn);
+	            		System.out.println("Dropped stored function " + fn + "()");
 	            } catch (SQLException ex) {
 	            		System.out.println("Did not drop function " + fn);
 	            }
@@ -93,61 +98,20 @@ public class OrderManager {
             		" DETERMINISTIC" +
             		" NO SQL" +
             		" EXTERNAL NAME " +
-            		"	'Helper.isSKU'"; 
+            		"	'edu.northeastern.cs_5200.Helper.isSKU'";
             stmt.executeUpdate(createFunction_isSKU);
-            System.out.println("Created function isSKU()");
-            
-            // create parseNumber function
-            String createFunction_parseNumber =
-            		"CREATE FUNCTION parseNumber(" + 
-            		"	s VARCHAR(64)" +
-            		") RETURNS INT" +
-            		" PARAMETER STYLE JAVA" +
-            		" LANGUAGE JAVA" +
-            		" DETERMINISTIC" +
-            		" NO SQL" +
-            		" EXTERNAL NAME " +
-            		"	'Helper.parseNumber'"; 
-            stmt.executeUpdate(createFunction_parseNumber);
-            System.out.println("Created function parseNumber()");
-            
-            // create parsePrice function
-            String createFunction_parsePrice =
-            		"CREATE FUNCTION parsePrice(" + 
-            		"	s VARCHAR(64)" +
-            		") RETURNS DECIMAL(18,2)" +
-            		" PARAMETER STYLE JAVA" +
-            		" LANGUAGE JAVA" +
-            		" DETERMINISTIC" +
-            		" NO SQL" +
-            		" EXTERNAL NAME " +
-            		"	'Helper.parsePrice'"; 
-            stmt.executeUpdate(createFunction_parsePrice);
-            System.out.println("Created function parsePrice()");
-            
-            // create parseDate function
-            String createFunction_parseDate =
-            		"CREATE FUNCTION parseDate(" + 
-            		"	s VARCHAR(64)" +
-            		") RETURNS DATE" +
-            		" PARAMETER STYLE JAVA" +
-            		" LANGUAGE JAVA" +
-            		" DETERMINISTIC" +
-            		" NO SQL" +
-            		" EXTERNAL NAME " +
-            		"	'Helper.parseDate'"; 
-            stmt.executeUpdate(createFunction_parseDate);
-            System.out.println("Created function parseDate()");
-            
+            System.out.println("Created stored function isSKU()");
             
             // create the Product table with SKU validation
             String createTable_Product =
             		  "CREATE TABLE Product ("
             		+ "  name VARCHAR(32) NOT NULL,"
             		+ "  description VARCHAR(64) NOT NULL,"
-            		+ "  SKU CHAR(12) NOT NULL," 
+            		+ "  SKU CHAR(12) NOT NULL,"
+					+ "  CONSTRAINT PK_SKU"
             		+ "  PRIMARY KEY (SKU),"
-            		+ "  CHECK (isSKU(SKU))," 
+					+ "  CONSTRAINT CHK_SKU"
+            		+ "  CHECK (isSKU(SKU))" 
             		+ ")";
             stmt.executeUpdate(createTable_Product);
             System.out.println("Created entity table Product");
@@ -157,10 +121,12 @@ public class OrderManager {
             		  "CREATE TABLE InventoryRecord ("
             		+ "  number INT NOT NULL,"
             		+ "  price DECIMAL(18,2) NOT NULL," 
-            		+ "  productSKU CHAR(12) NOT NULL," 
+            		+ "  productSKU CHAR(12) NOT NULL,"
+					+ "  CONSTRAINT PK_PRODUCT_SKU"
             		+ "  PRIMARY KEY (productSKU),"
+					+ "  CONSTRAINT FK_PRODUCT_SKU"
             		+ "  FOREIGN KEY (productSKU) REFERENCES Product (SKU) ON DELETE CASCADE,"
-            		+ "  CHECK (price > 0)" // check
+            		+ "  CHECK (price >= 0.0 and number >= 0)" // check
             		+ ")";
             stmt.executeUpdate(createTable_InventoryRecord);
             System.out.println("Created entity table InventoryRecord"); 
@@ -169,13 +135,16 @@ public class OrderManager {
             String createTable_Customer =
             		  "CREATE TABLE Customer ("
             		+ "  id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"
-            		+ "  name VARCHAR(20) NOT NULL,"
+            		+ "  name VARCHAR(32) NOT NULL,"
             		+ "  address VARCHAR(64) NOT NULL,"
             		+ "  city VARCHAR(16) NOT NULL,"
             		+ "  state VARCHAR(16) NOT NULL,"
             		+ "  country VARCHAR(16) NOT NULL,"
-            		+ "  zipCode INT NOT NULL,"
+            		+ "  postalCode VARCHAR(8) NOT NULL,"
+					+ "  CONSTRAINT PK_CUSTOMER_ID"
             		+ "  PRIMARY KEY (id),"
+					+ "  CONSTRAINT U_DUPLICATES"
+					+ "  UNIQUE(name, address, city, state, country, postalCode)"
             		+ ")";
             stmt.executeUpdate(createTable_Customer);
             System.out.println("Created entity table Customer");
@@ -187,8 +156,12 @@ public class OrderManager {
             		+ "  customerId INT NOT NULL,"
             		+ "  orderDate DATE NOT NULL,"
             		+ "  shipmentDate DATE," // can be null
+					+ "  CONSTRAINT PK_ORDER_ID"
             		+ "  PRIMARY KEY (id),"
-                + "  FOREIGN KEY (customerId) REFERENCES Customer (id) ON DELETE CASCADE" // check
+					+ "  CONSTRAINT FK_CUSTOMER_ID"
+                + "  FOREIGN KEY (customerId) REFERENCES Customer (id)," // check
+					+ "  CONSTRAINT CHK_DATES"
+					+ "  CHECK (shipmentDate is null or orderDate <= shipmentDate)"
             		+ ")";
             stmt.executeUpdate(createTable_TheOrder);
             System.out.println("Created relation table TheOrder"); 
@@ -200,10 +173,11 @@ public class OrderManager {
             		+ "  productSKU CHAR(12) NOT NULL,"
             		+ "  number INT NOT NULL,"
                 + "  inventoryPrice DECIMAL(18,2) NOT NULL," 
+					+ "  CONSTRAINT PK_ORDER_RECORD_ID"
         		    + "  PRIMARY KEY (orderId, productSKU),"
-                + "  FOREIGN KEY (orderId) REFERENCES Order (id),"
-                + "  FOREIGN KEY (productSKU) REFERENCES Product (SKU) ON DELETE CASCADE,"
-                + "  FOREIGN KEY (inventoryPrice) REFERENCES InventoryRecord (price) ON DELETE CASCADE"
+					+ "  CONSTRAINT FK_ORDER_ID"
+                + "  FOREIGN KEY (orderId) REFERENCES TheOrder (id),"
+                + "  FOREIGN KEY (productSKU) REFERENCES Product (SKU) ON DELETE CASCADE"
             		+ ")";
             stmt.executeUpdate(createTable_OrderRecord);
             System.out.println("Created relation table OrderRecord"); 
@@ -220,39 +194,6 @@ public class OrderManager {
 	            	+ "   WHERE InventoryRecord.productSKU = N_ROW.productSKU";
 			stmt.executeUpdate(createTrigger_OrderRecordInsert);
             System.out.println("Created trigger for inserting OrderRecord");
-            
-            // create trigger for deleting an order record that also updates (adds back) 
-            // number of units available for the InventoryRecord. The deletion of 
-            // an order record is caused by deletion on TheOrder (suppose the 
-            // customer cancels one of his orders // 反向trigger? check 
-			String createTrigger_OrderRecordDelete =
-         		  "CREATE TRIGGER OrderRecordDelete"
-         		+ " AFTER DELETE ON OrderRecord"
-         		+ " REFERENCING OLD AS DeletedOrderRecord"
-	    			+ " FOR EACH ROW MODE DB2SQL"
-	            	+ "   UPDATE InventoryRecord SET InventoryRecord.number = InventoryRecord.number + DeletedOrderRecord.number"
-	            	+ "   WHERE InventoryRecord.productSKU = DeletedOrderRecord.productSKU";
-			stmt.executeUpdate(createTrigger_OrderRecordDelete);
-            System.out.println("Created trigger for deleting OrderRecord");
-            
-            // create trigger for inserting an order record whose number of item
-            // is more than number of units available for the InventoryRecord, 
-            // thus resulting in the insertion failure, and the order itself 
-            // also gets canceled automatically. // check
-            String createTrigger_OrderRecordOverflow =
-           		  "CREATE TRIGGER OrderRecordOverflow"
-           		+ " BEFORE INSERT ON OrderRecord"
-           		+ " REFERENCING NEW AS N_ROW"
-  	    			+ " FOR EACH ROW MODE DB2SQL"
-  	    			+ " BEGIN"
-  	    			+ " IF N_ROW.number > InventoryRecord.number THEN" // check: MySQL syntax
-  	    			+ " DELETE FROM TheOrder WHERE id = N_ROW.orderId"
-  	            	+ "   " // check: how to cancel the insertion itself?
-  	            	+ " END IF;"
-  	            	+ " END;";
-  			stmt.executeUpdate(createTrigger_OrderRecordOverflow);
-            System.out.println("Created trigger for inserting OrderRecord"); // check
-              
             
 		} catch (SQLException e) {
 			e.printStackTrace();
